ffff06403f7a0976751be83873f74db3a26cfdd7
Edwin Mwaniki <edwin.mwaniki@student.moringaschool.com>
2025-06-07 16:27:54 +0300

    Completed BodaFareCalcutor

--- bodaFareCalculator/fare.js
+++ bodaFareCalculator/fare.js
@@ -1,1 +1,17 @@
+let distanceInKm = prompt("Unafika wapi? Kilometer ngapi?");
+let baseFare = 50;
+let chargePerKm = 15;
+let i = parseFloat(distanceInKm);
 
+if (isNaN(i)===true) {
+    alert('ERROR: The value Entered is not a number');
+    throw new Error();
+}
+
+let totalFare = baseFare + (i*chargePerKm);
+
+console.log('Uko kwote? Io ni '+i+' km');
+console.log('Ukikalia Pikipiki: KES '+baseFare);
+console.log('Mpaka Uko: KES '+(i*chargePerKm));
+console.log('Total: KES '+totalFare);
+console.log('Panda Pikipiki!');
