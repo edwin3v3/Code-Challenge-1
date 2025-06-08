da72ac3e4d87d034787387737a8b46ff9a79aa8d
Edwin Mwaniki <edwin.mwaniki@student.moringaschool.com>
2025-06-08 23:42:10 +0300

    Edit the Comments

--- chaiCalculator/chai.js
+++ chaiCalculator/chai.js
@@ -1,16 +1,22 @@
-let numberofCups = prompt("Karibu! How many cups of Chai Bora would you like to make?");
-let i = parseInt(numberofCups);
+/**
+ * Javascipt program to calculate the total ingredients required to-
+ *  make a certain amount of cups of tea
+ */
+let numberofCups = prompt("Karibu! How many cups of Chai Bora would you like to make?"); // variable declaration & prompt to assign value
+let i = parseInt(numberofCups); // ensure the value assigned is number
 
+// error checking of the value assigned with pop up alert
 if (isNaN(i)===true) {
     alert('ERROR: The value Entered is not a whole number');
     throw new Error();
 }
-
+// assign formulas for each ingredient variable
 let waterAmt = i*200;
 let milkAmt = i*50;
 let teaAmt = i*1;
 let sugarAmt = i*2;
 
+// print the ingredients required
 console.log('To make '+i+' cups of Kenyan Chai, you will need:');
 console.log('Water: '+waterAmt+'ml');
 console.log('Milk: '+milkAmt+'ml');
